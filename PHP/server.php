0126d5350eabce2695c8708ef6acbb447297a2bf
jarji <100996174+jarjia@users.noreply.github.com>
2023-07-17 12:37:31 +0400

    feat: fix php file

--- PHP/server.php
+++ PHP/server.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:5173, https://jarjia.github.io/portfolio/');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Max-Age: 86400');
