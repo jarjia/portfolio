ecea43f2289c97d8d39d412db35a2cb70a3e7599
jarji <100996174+jarjia@users.noreply.github.com>
2023-11-08 19:36:15 +0400

    feat: preload images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import { Layout } from './components'
+import { loadImage } from './helpers'
+import { useEffect } from 'react'
 import { Home } from './pages'
+import sun from 'images/sun.png'
+import ghLogo from 'images/gh-logo.png'
+import hosted from 'images/hosted.png'
+import moon from 'images/moon.png'
+import nightBg from 'images/night-bg.avif'
+import sunBg from 'images/sun-bg.avif'
 
 function App() {
+  useEffect(() => {
+    loadImage([sun, ghLogo, hosted, moon, nightBg, sunBg])
+  }, [])
+
   return (
     <Layout>
       <Home />
